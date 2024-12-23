223bb069bc8c2736bf203fb7b353ac81cc3b2c7c
William Magnabosco <williamlodea@gmail.com>
2024-12-23 19:40:31 -0400

    remove content security polices

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -28,8 +28,3 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
-
-Rails.application.config.content_security_policy do |policy|
-    policy.frame_ancestors "'*'"
-  end
-  
