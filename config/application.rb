e5013f3e0ec31be41ce808fc2a7e588b4ee65d56
William Magnabosco <williamlodea@gmail.com>
2024-12-30 14:55:56 -0400

    added permissions policy

--- config/application.rb
+++ config/application.rb
@@ -59,15 +59,10 @@ module Chatwoot
     # FIX ME : fixes breakage of installation config. we need to migrate.
     config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess]
 
-    # config.action_dispatch.default_headers = {
-    #   'X-Frame-Options' => 'ALLOWALL'
-    # }
-    config.action_dispatch.default_headers.merge!({
+    config.action_dispatch.default_headers = {
       'X-Frame-Options' => 'ALLOWALL',
-      'Content-Security-Policy' => "frame-ancestors *;"
-      # 'Content-Security-Policy' => "frame-ancestors 'self' https://your-trusted-domain.com;"
-    })
-
+      'Permissions-Policy' => "fullscreen=(), geolocation=(), camera=()"
+    }
   end
 
   def self.config
